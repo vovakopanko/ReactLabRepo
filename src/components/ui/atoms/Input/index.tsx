b21ff04bef2b1d5762764cd5d426e76567bb1688
Vladzimir <58780728+vovakopanko@users.noreply.github.com>
2022-01-26 18:42:30 +0300

    fix: work debounce

--- src/components/ui/atoms/Input/index.tsx
+++ src/components/ui/atoms/Input/index.tsx
@@ -1,40 +1,27 @@
 import { searchAPI } from "@/api/SearchAPI";
+import { debounce } from "lodash";
 import { FC, useEffect, useState } from "react";
 import { TGameCard } from "../../organisms/GameList/types";
 import ConditionalRenderList from "./ConditionalRenderList";
 import { FinderContainer, StyleInput } from "./style";
 
+const fetchData = async (query: string, cb: (val: TGameCard[]) => void) => {
+  const res = await searchAPI.getGameCards(`&q=${query}`);
+  cb(res);
+};
+
+const debouncedFetchData = debounce((query, cb) => {
+  fetchData(query, cb);
+}, 300);
+
 const Input: FC = () => {
   const [searchData, setSearchData] = useState("");
   const [findArray, setFindArray] = useState<TGameCard[]>([]);
-  const [toggle, setToggle] = useState(true);
+  const [isFocus, setIsFocus] = useState(false);
 
   useEffect(() => {
-    const query = async () => {
-      if (searchData) {
-        const data = await searchAPI.getGameCards(`&q=${searchData}`);
-        setFindArray(data);
-      }
-    };
-    query();
+    debouncedFetchData(searchData, (res: TGameCard[]) => setFindArray(res));
   }, [searchData]);
-
-  console.log("1fdf", findArray);
-
-  // don't work debounce/Throttling
-  // added other libraries, but there were no changes
-
-  const debounce = (fn: any, delay: any) => {
-    let timeoutId: any;
-    return function (...args: any[]) {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-      timeoutId = setTimeout(() => {
-        fn(...args);
-      }, delay);
-    };
-  };
 
   return (
     <FinderContainer>
@@ -42,21 +29,21 @@ const Input: FC = () => {
         type="text"
         name="searchTerm"
         autoComplete="off"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          debounce(setSearchData(e.target.value.trim()), 300);
-          // setSearchData(e.target.value.trim());
-          setToggle(true);
+        onFocus={() => {
+          setIsFocus(true);
         }}
-        // onBlur={() => setToggle(false)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setSearchData(e.target.value.trim());
+          setIsFocus(true);
+        }}
         value={searchData}
-        placeholder={searchData}
       />
       <ConditionalRenderList
         value={searchData}
         list={findArray}
         setValue={setSearchData}
-        toggle={toggle}
-        setToggle={setToggle}
+        toggle={isFocus}
+        setToggle={setIsFocus}
       />
     </FinderContainer>
   );
